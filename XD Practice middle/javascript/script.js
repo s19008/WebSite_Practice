b3b07c5b53f269ba610c406a2db19e4a7d47217b
s19008 <s19008@std.it-college.ac.jp>
2021-02-13 23:55:13 +0900

    Copy Practice

--- XD Practice middle/javascript/script.js
+++ XD Practice middle/javascript/script.js
@@ -1,18 +1,25 @@
-$(document).ready(function () {
-
-    const $submitBtn = $('#js-submit')
-    $('#form input,#form textarea').on('change', function() {
-        if (
-            $('#form input[type="text"]').val() !== "" &&
-            $('#form input[type="email"]').val() !== "" &&
-            $('#form input[type="checkbox"]').val() !== "" &&
-            $('#form #privacyCheck').prop('checked') === true
-        ) {
-            $submitBtn.prop('disabled', false); 
-            $submitBtn.css('opacity', 1);
-        } else {
-            $submitBtn.prop('disabled', true);
-        }
+$(function () {
+    const $question = $('.faq-inner-copy .question')
+    $question.on('click',function(){
+        $(this).next().slideToggle(100);
     });
+    $(document).ready(function () {
 
+        const $submitBtn = $('#js-submit')
+
+        $('#form input,#form textarea').on('change', function () {
+            if (
+                $('#form input[type="text"]').val() !== "" &&
+                $('#form input[type="email"]').val() !== "" &&
+                $('#form textarea').val() !== "" &&
+                $('#form input[type="checkbox"]').val() !== "" &&
+                $('#form #privacyCheck').prop('checked') === true
+            ) {
+                $submitBtn.prop('disabled', false);
+                $submitBtn.css('opacity', 1);
+            } else {
+                $submitBtn.prop('disabled', true);
+            }
+        });
+    });
 });
