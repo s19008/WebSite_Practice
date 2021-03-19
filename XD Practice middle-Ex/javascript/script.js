39d863b73720726c63e1e479a4c9b76286845caa
s19008 <s19008@std.it-college.ac.jp>
2021-03-20 00:01:23 +0900

    Practice

--- XD Practice middle-Ex/javascript/script.js
+++ XD Practice middle-Ex/javascript/script.js
@@ -1,6 +1,7 @@
 $(function(){
     $('.btn-trigger').on('click', function() {
       $(this).toggleClass('active');
+      $('.header-inner-sp-nav').toggleClass(500);
       return false;
     });
   });
