4dd5582d7100a751073ce831a25aba0e0591222b
s19008 <s19008@std.it-college.ac.jp>
2021-03-22 18:19:13 +0900

    Practice

--- XD Practice middle-Ex/javascript/script.js
+++ XD Practice middle-Ex/javascript/script.js
@@ -1,7 +1,27 @@
 $(function(){
-    $('.btn-trigger').on('click', function() {
-      $(this).toggleClass('active');
-      $('.header-inner-sp-nav').toggleClass(500);
+    var btnTrigger = $('.btn-trigger');
+
+    btnTrigger.on('click', function() {
+      btnTrigger.toggleClass('active');
+      $('.header-inner-sp-nav').fadeToggle(500);
+      $('.fix-header-inner-sp-nav').fadeToggle(500);
       return false;
     });
+    
+    var _window = $(window),
+        _header = $('.header'),
+        _headerChange = $('.fix-header'),
+        heroBottom;
+    
+    _window.on('scroll',function(){
+      heroBottom = $(_header).height();
+      if(_window.scrollTop() > heroBottom){
+        _headerChange.addClass('show');
+      }
+      else{
+        _headerChange.removeClass('show');
+      }
+    });
+    _window.trigger('scroll')
+        
   });
