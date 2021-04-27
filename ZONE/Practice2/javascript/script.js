71e9fd89cfee592d95119f29ec35bb865a816c4b
s19008 <s19008@std.it-college.ac.jp>
2021-04-28 01:57:25 +0900

    Practice

--- ZONE/Practice2/javascript/script.js
+++ ZONE/Practice2/javascript/script.js
@@ -5,13 +5,4 @@ $(function() {
         $(".header-sp-nav").fadeToggle(500);
         return false;
     });
-    let mySwiper = new Swiper('.swiper-container', {
-        loop: true,
-        centeredSlides: true,
-        slidesPerView: 3.5,
-        spaceBetween: 56,
-        autoplay: {
-            delay: 5000,
-        },
-    });
 });
