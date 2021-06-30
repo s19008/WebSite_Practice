e9d007bf8da5485c2d98b58141a6c3f668cb974f
s19008 <s19008@std.it-college.ac.jp>
2021-07-01 00:20:08 +0900

    Practice

--- ZONE/lastPractice/src/javascript/script.js
+++ ZONE/lastPractice/src/javascript/script.js
@@ -1,9 +1,27 @@
 $(function() {
     let btnTrigger = $(".btn-trigger");
     let overlay = $(".overlay");
+    let nav_a = $('.nav-a');
+    let hr = $('hr');
     btnTrigger.on("click", function() {
         btnTrigger.toggleClass("active");
         overlay.toggleClass("open");
         return false;
     });
-});
+
+    window.onload = function() {
+        const spinner = document.getElementById('load');
+        spinner.classList.add('loaded');
+    };
+    
+    $(window).scroll(function() {
+        let scroll = $(this).scrollTop();
+        if(scroll > 1660) {
+            nav_a.css('color', '#000');
+            hr.css('background-color', '#000');
+        } else {
+            nav_a.css('color', '#FFF');
+            hr.css('background-color', '#FFF');
+        };
+    });
+}); 
